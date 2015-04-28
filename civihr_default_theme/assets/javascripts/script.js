bda21b3b787024e5e7ef253a34338ab6c6819fb1
Cezar Derevlean <c3zar22@gmail.com>
2015-04-28 16:43:02 +0300

    PCHR-221 Fix dropdown styles.

--- civihr_default_theme/assets/javascripts/script.js
+++ civihr_default_theme/assets/javascripts/script.js
@@ -3,9 +3,42 @@
  * Custom scripts for theme.
  */
 (function ($) {
-  $(document).ready(function() {
+    // on doc ready
+    $(document).ready(function() {
 
     $('.view-hr-vacancies li').matchHeight();
 
-  });
+        applyCustomSelect();
+    });
+
+    // on ajax complete (ie: when opening modals)
+    $(document).ajaxComplete(function() {
+
+        applyCustomSelect();
+
+    });
+
+    function applyCustomSelect() {
+        initCustomSelect();
+        onFieldsetLegendClick();
+    }
+
+    // when toggling expanding areas, apply customSelect to unaffected select elements
+    function onFieldsetLegendClick() {
+        $('.fieldset-legend .fieldset-title').click(initCustomSelect);
+    }
+
+    // initialize customSelect for unaffected visible select elements
+    function initCustomSelect() {
+        $('.form-item select').not('.hasCustomSelect').filter(':visible').each(function() {
+            var $this = $(this);
+            if ( $('body').hasClass('page-dashboard') ) {
+                if ( !$this.parent().parent().hasClass('views-widget') ) {
+                    $this.customSelect();
+                }
+            } else {
+                $this.customSelect();
+            }
+        });
+    }
 })(jQuery);

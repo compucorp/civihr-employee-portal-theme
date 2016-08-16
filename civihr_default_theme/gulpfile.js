4ba17d409eb5ee9c721af2669031b6ffae31cffe
Debarshi Bhaumik <deb1990@gmail.com>
2016-08-16 12:19:54 +0530

    PCHR-1316: Gulp output style changed to generate compressed CSS

--- civihr_default_theme/gulpfile.js
+++ civihr_default_theme/gulpfile.js
@@ -34,7 +34,7 @@ gulp.task('css', function() {
       }}))
     .pipe(sourcemaps.init())
     .pipe(sass({
-      style: 'compressed',
+      outputStyle: 'compressed',
       errLogToConsole: true,
       includePaths: config.css.includePaths
     }))

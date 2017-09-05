a90cf704cada8c434337e4c27b4c6befa5b58b9c
mickadoo <michaeldevery@gmail.com>
2017-09-05 17:03:27 +0100

    PCHR-2339: Add progress bar to user edit onboarding template

--- civihr_default_theme/templates/user/page--user--edit--onboarding.tpl.php
+++ civihr_default_theme/templates/user/page--user--edit--onboarding.tpl.php
@@ -1,13 +1,17 @@
 <?php
 
-/** @var array $page Contains all variables and details for the page */
+/**
+ * @var array $page Contains all variables and details for the page
+ * @var string $container_class
+ */
+
 if (isset($page['content']['system_main'])) {
   $form = &$page['content']['system_main'];
 } else {
   $form = [];
 }
 
-// hide all form parts except account and actions
+// Hide all form parts except account and actions
 $formParts = element_get_visible_children($form);
 $visibleParts = ['account', 'actions', 'form_token'];
 $hiddenParts = array_diff($formParts, $visibleParts);
@@ -15,13 +19,32 @@ foreach ($hiddenParts as $part) {
   hide($form[$part]);
 }
 
-// hide all account parts except password
+// Hide all account parts except password
 $accountParts = element_get_visible_children($form['account']);
 $visibleAccountParts = ['pass', 'current_pass'];
 $hiddenAccountParts = array_diff($accountParts, $visibleAccountParts);
 foreach ($hiddenAccountParts as $accountPart) {
   hide($form['account'][$accountPart]);
 }
+
+// Change the label on the form button
+if (isset($page['content']['system_main']['actions']['submit']['#value'])) {
+  $page['content']['system_main']['actions']['submit']['#value'] = 'Next';
+}
+
+// Load the progress bar
+$criteria = ['title' => 'Welcome to CiviHR', 'type' => 'webform'];
+$node = current(node_load_multiple(NULL, $criteria));
+$pageLabels = webform_page_labels($node);
+array_unshift($pageLabels, 'Password');
+$variables = [
+  'node' => $node,
+  'page_num' => 1,
+  'page_count' => count($pageLabels),
+  'page_labels' => $pageLabels,
+];
+$progressBar = theme('webform_progressbar', $variables);
+
 ?>
 
 <div id="outer-wrapper">
@@ -33,15 +56,14 @@ foreach ($hiddenAccountParts as $accountPart) {
             <?php print $messages; ?>
           </div>
         <?php endif; ?>
-        <div id="page-header">
-          <?php if ($title): ?>
-            <div class="page-header">
-              <h1 class="title"><?php print $title; ?></h1>
-            </div>
-          <?php endif; ?>
-        </div>
       </div>
+
       <div id="content" class="<?php print $container_class; ?> collapse">
+
+        <div class="progress_bar">
+          <?php print $progressBar; ?>
+        </div>
+
         <div class="main-container">
           <?php print render($page['content']); ?>
         </div>
